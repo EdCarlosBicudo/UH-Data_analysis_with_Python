dbeed2ff7eb66e6a8a604cd00890911cecb58f79
Ed Carlos Bicudo <ed-carlo@hotmail.com>
2021-06-14 16:29:03 -0300

    Exercícios 7, 8 e 9 resolvidos

--- hy-data-analysis-with-python-summer-2021/part01-e07_areas_of_shapes/src/areas_of_shapes.py
+++ hy-data-analysis-with-python-summer-2021/part01-e07_areas_of_shapes/src/areas_of_shapes.py
@@ -1,10 +1,32 @@
-#!/usr/bin/env python3
-
 import math
 
 
 def main():
-    # enter you solution here
+    areas = {'triangle': lambda b, h: (b*h)/2,
+             'rectangle': lambda w, h: (w*h),
+             'circle': lambda r: math.pi * (r ** 2)}
+
+    while True:
+        shape = input("Choose a shape (triangle, rectangle, circle): ")
+
+        if not shape:
+            break
+
+        if shape not in areas.keys():
+            print("Unknown shape!")
+            continue
+
+        args = []
+        if shape == "triangle":
+            args.append(int(input("Give base of the triangle: ")))
+            args.append(int(input("Give height of the triangle: ")))
+        if shape == "rectangle":
+            args.append(int(input("Give width of the rectangle: ")))
+            args.append(int(input("Give height of the rectangle: ")))
+        if shape == "circle":
+            args.append(int(input("Give radius of the circle: ")))
+
+        print(f"The area is {areas[shape](*args)}")
 
 if __name__ == "__main__":
     main()
--- hy-data-analysis-with-python-summer-2021/part01-e09_merge/src/merge.py
+++ hy-data-analysis-with-python-summer-2021/part01-e09_merge/src/merge.py
@@ -1,10 +1,33 @@
-#!/usr/bin/env python3
 
 def merge(L1, L2):
-    return []
+    cL1 = L1.copy()
+    cL2 = L2.copy()
+
+    lenght = len(cL1) + len(cL2)
+
+    final = []
+    for _ in range(lenght):
+        n = None
+
+        if len(cL1) > 0:
+            if len(cL2) > 0:
+                if cL1[0] < cL2[0]:
+                    n = cL1.pop(0)
+                else:
+                    n = cL2.pop(0)
+            else:
+                n = cL1.pop(0)
+        else:
+            n = cL2.pop(0)
+
+        final.append(n)
+
+    return final
 
 def main():
-    pass
+    l1 = [1, 5, 9, 12]
+    l2= [2, 6, 10]
+    print(merge(l1, l2))
 
 if __name__ == "__main__":
     main()
