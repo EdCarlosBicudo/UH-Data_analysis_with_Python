afd8978039edf087fd423b082b02d9fc2348015c
Ed Carlos Bicudo <ed-carlo@hotmail.com>
2021-06-28 14:05:09 -0300

    Resolvidos do 16 e 17

--- hy-data-analysis-with-python-summer-2021/part04-e17_cleaning_data/src/cleaning_data.py
+++ hy-data-analysis-with-python-summer-2021/part04-e17_cleaning_data/src/cleaning_data.py
@@ -1,14 +1,21 @@
-#!/usr/bin/env python3
 
 import pandas as pd
 import numpy as np
 
 
 def cleaning_data():
-    return None
+    df = pd.read_csv("src/presidents.tsv", sep="\t")
+    df["President"] = df["President"].str.replace(r"(\w+), *(\w+)", r"\2 \1").str.title()
+    df["Last"] = pd.to_numeric(df["Last"], errors='coerce')
+    df["Vice-president"] = df["Vice-president"].str.replace(r"(\w+), *(\w+)", r"\2 \1").str.title()
+    df["Start"] = df["Start"].str.split(" ", expand=True)[:][0]
+    df["Seasons"] = df["Seasons"].str.replace('two', '2', regex=False)
+    df = df.astype({"President": object, "Start": int, "Last": float, "Seasons": int, "Vice-president": object})
+    return df
 
 def main():
-    return
+    df = cleaning_data()
+    print(df)
 
 if __name__ == "__main__":
     main()
