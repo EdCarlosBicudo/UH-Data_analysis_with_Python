5dd7f60475a1b6ae2393f872a116aa74b76a80e2
Ed Carlos Bicudo <ed-carlo@hotmail.com>
2021-08-16 15:43:37 -0300

    Resolvido do 10 ao 13

--- hy-data-analysis-with-python-summer-2021/part05-e12_coefficient_of_determination/src/coefficient_of_determination.py
+++ hy-data-analysis-with-python-summer-2021/part05-e12_coefficient_of_determination/src/coefficient_of_determination.py
@@ -1,13 +1,31 @@
-#!/usr/bin/env python3
+
 
 import pandas as pd
 from sklearn import linear_model
 
 
 def coefficient_of_determination():
-    return []
-    
+    df = pd.read_csv("src/mystery_data.tsv", sep="\t")
+    X = df.iloc[:,0:5]
+    y = df.iloc[:,5]
+    model = linear_model.LinearRegression()
+    model.fit(X, y)
+    scores = []
+    scores.append(model.score(X, y))
+    for i in X:
+        model.fit(df[i].values.reshape(-1, 1), y)
+        score = model.score(df[i].values.reshape(-1, 1), y)
+        scores.append(score)
+    return scores
+
 def main():
+    scores = coefficient_of_determination()
+    for i, n in enumerate(scores):
+        if i==0:
+            print(f"R2-score with feature(s) X: {n}")
+        else:
+            print(f"R2-score with feature(s) X{i+1}: {n}")
+
 
 if __name__ == "__main__":
     main()
